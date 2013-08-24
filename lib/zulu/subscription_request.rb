ee73d51b398c8f4ed679e7d51e5515aca06f22ed
Dan Weinand <dweinand@gmail.com>
2013-08-24 10:57:56 -0700

    reorganzing validations

--- lib/zulu/subscription_request.rb
+++ lib/zulu/subscription_request.rb
@@ -34,24 +34,6 @@ module Zulu
       errors.map {|e| "hub.#{e[0]} #{e[1]}" }
     end
     
-    def validate_mode
-      @mode or errors << [:mode, 'must be present']
-      in_list = ['subscribe','unsubscribe'].include?(@mode)
-      in_list or errors << [:mode, "must be either 'subscribe' or 'unsubscribe'"]
-    end
-    
-    def validate_topic
-      @topic or errors << [:topic, 'must be present']
-    end
-    
-    def validate_callback
-      @callback or errors << [:callback, 'must be present']
-      uri = Addressable::URI.parse(@callback)
-      uri and %w(http https).include? uri.scheme or fail Addressable::URI::InvalidURIError
-    rescue Addressable::URI::InvalidURIError
-      errors << [:callback, 'must be a valid http url']
-    end
-    
     def save
       valid? and Zulu.redis.lpush(LIST, to_json)
     end
@@ -69,6 +51,26 @@ module Zulu
     
     def ==(other)
       to_hash == other.to_hash
+    end
+    
+    private
+    
+    def validate_mode
+      @mode or errors << [:mode, 'must be present']
+      in_list = ['subscribe','unsubscribe'].include?(@mode)
+      in_list or errors << [:mode, "must be either 'subscribe' or 'unsubscribe'"]
+    end
+    
+    def validate_topic
+      @topic or errors << [:topic, 'must be present']
+    end
+    
+    def validate_callback
+      @callback or errors << [:callback, 'must be present']
+      uri = Addressable::URI.parse(@callback)
+      uri and %w(http https).include? uri.scheme or fail Addressable::URI::InvalidURIError
+    rescue Addressable::URI::InvalidURIError
+      errors << [:callback, 'must be a valid http url']
     end
     
   end
